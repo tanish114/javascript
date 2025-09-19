b7626fb5e5bfe4e85ea30c5a538279c2b4e2e050
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-09-19 16:25:05 +0530

    new

---
+++ script.js
@@ -0,0 +1,26 @@
+//  console.log("this is a js file")
+
+//  alert("this is a alert message")
+
+// document.write("hello")
+
+
+// var num=12
+// var num = 30
+// console.log(num)
+
+// let n1=90
+
+// n1="redhat"
+// console.log(n1)
+
+
+// const name="tanish"
+
+// console.log(name)
+
+n1=parseInt(prompt("enter firdt number"))
+n2=parseInt(prompt("enter firdt number"))
+console.log(n1+n2);
+console.log(n1*n2);
+
