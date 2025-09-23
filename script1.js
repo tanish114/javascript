4648ccdd169462938ec492c55752e9d302a9f5b6
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-09-23 16:28:13 +0530

    new

--- script1.js
+++ script1.js
@@ -44,15 +44,157 @@
 
 // wap three input from user check 2nd input is even or odd. if second number is even then print multiplication of three input
 
-n1=parseInt(prompt("enter a number"))
-n2=parseInt(prompt("enter a number"))
-n3=parseInt(prompt("enter a number"))
+// let n1=parseInt(prompt("enter a number"))
+// let n2=parseInt(prompt("enter a number"))
+// let n3=parseInt(prompt("enter a number"))
 
-if (n2%2==0) {
-    console.log(n1*n2*n3);
+// if (n2%2==0) {
+//     console.log(n1*n2*n3);
     
-}
-else {
-    console.log(n1+n2+n3);
+// }
+// else {
+//     console.log(n1+n2+n3);
     
-}
+// }
+
+// ladder if else
+
+    // syntax
+// if(condition) {
+//  code
+//   }
+// else if(consition) {
+// code
+// }
+// else {
+    // }
+
+    // question +ve,-ve,zero
+// n1=parseInt(prompt("enter a number"))
+// if(n1>0) {
+//     console.log("psoitive");
+    
+// }
+// else if(n1<0) {
+//     console.log("negative");
+    
+// }
+// else{
+//     console.log("neutral");
+    
+// }
+
+
+// check which is greatest or is equal
+
+
+// n1=parseInt(prompt("enter a number"))
+// n2=parseInt(prompt("enter a number"))
+
+// if(n1>n2) {
+//     console.log("n1 is greater");
+    
+// }
+// else if(n1<n2) {
+//     console.log("n2 is greater")
+// }
+// else{
+//     console.log("equal")
+// }
+
+// nested if else 
+
+// question 1
+// let age=parseInt(prompt("enter your age"))
+
+// if (age>=18) {
+//     let b=parseInt(prompt("press 1 if you are indian"))
+//     if(b==1) {
+//         console.log("you are eligible to vote");
+//     }
+//     else {
+//         alert("not inidan");
+//     }
+// } 
+// else {
+//     alert("underage")
+// }
+
+// question 2f
+// a=parseInt(prompt("enter your number"))
+
+// if(a>0) {
+// if(a%2==0) {
+//     console.log("square is ",a*a)
+// }
+// else{
+//     console.log("area of circle is ",3.14*a*a)
+// }
+// }
+// else{
+// if(a==-3) {
+//     console.log(a)
+// }
+// else {
+//     console.log("not equal to -3")
+// }
+// }
+
+// let  a=10
+// do{
+//     console.log("tv");
+//     a++
+// } while(a<10)
+
+// let a=10
+// do{
+//     console.log(a)
+//     a--
+// } while(a>=1)
+
+let a=parseInt(prompt("enter a number"))
+do {
+   a
+} while(a ==0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
