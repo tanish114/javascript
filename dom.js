b9e093073773f192d86f88d45e356b405e8bc01b
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-10-06 16:26:20 +0530

    new

--- dom.js
+++ dom.js
@@ -5,3 +5,33 @@
 // every element(tag) is known as node in dom.
 // if we are targeting text in html it is known as text node in dom 
 // everthing is object in dom
+// if we want to provide functionality to any html action then we must have to first target that thing on js
+// getelementbyid is a function
+// textcontent changes or tqarget text
+// inner html text can be change but we can also apply tag with it
+// to target body then we use document.body
+// setattribute a function that always takes two parameter- 1-attribute name =,2-attribute value
+// we can perform action on particular element by setattribute on a particular variable
+
+
+//  document.getElementById('head1').textContent="1st js"
+//  document.getElementById('head2').textContent="2nd js"
+//  document.getElementById('head2').textContent="3rd js"
+//  document.getElementById('head4').textContent="4th js"
+//  document.getElementById('head5').textContent="5th js"
+//  document.getElementById('head6').textContent="6th js"
+
+// let a = document.getElementById('demo')
+// a.textContent="hello CYBROM"
+
+
+// let b= document.getElementById('lily')
+// b.innerHTML=" <i> hello sir </i>"
+
+ let b= document.getElementById('demo')
+ b.innerHTML=" <u> hello sir </u>"
+ b.setAttribute('align','center')
+document.getElementById('demo').removeAttribute('style')
+
+
+document.body.setAttribute('bgcolor','yellow')
