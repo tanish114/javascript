938500b7789cffb86f1f30d0e187132775acdb8f
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-10-08 16:28:21 +0530

    new

---
+++ dom3.js
@@ -0,0 +1,48 @@
+// we can chnage image set in html by targeting src in js
+
+// document.getElementById('image').src="image2.avif"
+
+
+
+// events are called actions which are performed by user on our webpage
+// mouse event
+// onclick works when click ( click left click only)
+
+// ondblclick works when double click(click double left click only)
+
+// oncontextmenu works when single right click
+
+// onmouseenter works when mouse is brought in it and will keep on working even when mouse is removed from it
+
+// onmouseleave works when mouse is leave
+
+// function fun(){
+//     alert("working....")
+// }
+
+
+// function red(){
+//     document.body.style.backgroundColor="red"
+    
+// }
+// function blue(){
+//     document.body.style.backgroundColor="blue"
+    
+// }
+// function yellow(){
+//     document.body.style.backgroundColor="yellow"
+    
+// }
+// function orange(){
+//     document.body.style.backgroundColor="orange"
+    
+// }
+
+//  or by single variable
+// function color(cl){
+//     document.body.style.backgroundColor=cl
+// }
+
+function fun1(){
+    document.getElementById('head').textContent="chnaged by js"
+}
