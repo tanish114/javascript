efe55bd54207931f5faef17c25b2763aaa6a787d
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-10-15 15:28:23 +0530

    new

--- callback.js
+++ callback.js
@@ -3,21 +3,35 @@
 
 // setInterval(()=>{document.write("hii....")},3000)
 
-let a=0
-let h1=document.querySelector('#count')
-let st
-function fun1(){
-    st=setInterval(()=>{
-    h1.innerHTML= ++a
-    },1000)
-}
 
-function stop(){
-    clearInterval(st)
-}
+// Write a JavaScript program using setInterval()
+//  that prints numbers from 1 to 5 in the console every 1 second.
 
-function reset(){
-    a=0
-    h1.innerHTML=a
-    clearInterval(st)
-}
+// let count=1
+// let a=setInterval(()=>{
+//     console.log(count);
+//     count++;
+//     if(count>5){
+//         clearInterval(a)
+//     }
+// },1000)
+
+// let a=0
+// let h1=document.querySelector('#count')
+// let st
+// function fun1(){
+//     st=setInterval(()=>{
+//     h1.innerHTML= ++a
+//     },1000)
+// }
+
+// function stop(){
+//     clearInterval(st)
+// }
+
+// function reset(){
+//     a=0
+//     h1.innerHTML=a
+//     clearInterval(st)
+// }
+
