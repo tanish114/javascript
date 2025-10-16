1e28b02566629a438138e7b24877304d4a515e66
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-10-16 15:57:24 +0530

    new

--- callback.js
+++ callback.js
@@ -6,6 +6,35 @@
 
 // Write a JavaScript program using setInterval()
 //  that prints numbers from 1 to 5 in the console every 1 second.
+// let count=1
+// let a=setInterval(()=>{
+    
+// if(count==1){
+//     alert("first alert")
+//     count++
+// }
+// else if(count==2){
+//     alert("second alert")
+//     count++
+// }
+// else if(count==3){
+//     alert("third alert")
+//     count++
+// }
+// else if(count==4){
+//     alert("fourth alert")
+//     count++
+// }
+// else if(count==5){
+//     alert("fifth alert")
+//     count++
+// }
+// else{
+//     clearInterval(a)
+// }
+
+
+},3000)
 
 // let count=1
 // let a=setInterval(()=>{
