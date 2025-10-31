400014630223d933c0e2916fe97889a325d1fd58
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-10-31 15:27:21 +0530

    new

--- formval.js
+++ formval.js
@@ -4,23 +4,23 @@
 
 
 
-function validate(){
-    let name=document.querySelector('#name').value
-    let age=document.querySelector('#age').value
-    let city=document.querySelector('#city').value
-    let salary=document.querySelector('#salary').value
-    let sub=document.querySelector('#sub').value
+// function validate(){
+//     let name=document.querySelector('#name').value
+//     let age=document.querySelector('#age').value
+//     let city=document.querySelector('#city').value
+//     let salary=document.querySelector('#salary').value
+//     let sub=document.querySelector('#sub').value
 
-if(name=="") {
-    document.querySelector('#error').innerHTML="please fill the detail"
-    document.querySelector('#name').focus()
-    return false
-}
-else if(city=="") {
-    alert("fill your city")
-    document.querySelector('#city').focus()
-    return false
-}
+// if(name=="") {
+//     document.querySelector('#error').innerHTML="please fill the detail"
+//     document.querySelector('#name').focus()
+//     return false
+// }
+// else if(city=="") {
+//     alert("fill your city")
+//     document.querySelector('#city').focus()
+//     return false
+// }
 
 // else if(age=="") {
 //     alert("fill your age")
@@ -38,21 +38,21 @@ else if(city=="") {
 
 
 
-else if(isNaN(salary)) {
-    alert("fill your salary inn numbers")
-    document.querySelector('#salary').focus()
-    return false
-}
+// else if(isNaN(salary)) {
+//     alert("fill your salary inn numbers")
+//     document.querySelector('#salary').focus()
+//     return false
+// }
 
-else if(sub=="") {
-    alert("fill your sub")
-    document.querySelector('#sub').focus()
-    return false
-}
+// else if(sub=="") {
+//     alert("fill your sub")
+//     document.querySelector('#sub').focus()
+//     return false
+// }
 
 
 
-}
+// }
 
 
 
