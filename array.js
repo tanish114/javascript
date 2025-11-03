cb52579eb6945354c031c0730f08ca9115833c94
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-11-03 15:32:02 +0530

    new

---
+++ array.js
@@ -0,0 +1,43 @@
+// array is a collection of multiple types of data
+// array is ordered as it has indexing
+// need of array is when we want to store multiple type of data in single variable
+// array is denoted by []
+// for loop always wprk on indexing when it comes to array
+
+// let student=["amit",22,"bhopal","ips",5.9,true]
+// console.log(student[3])
+
+
+
+
+// let emp=[]
+// for(let i=0;i<5;i++){
+//     emp[i]=prompt("enter")
+    
+// }
+
+// console.log(emp)
+
+// wap to take 10 input from user and store in array then print the sum of all element in array
+
+// let ele=[]
+// let sum =0 
+// for(let i=0;i<11;i++){
+//     ele[i]=parseInt(prompt("enter element"))
+    
+//     sum = sum + ele[i]
+    
+// }
+
+let ele=[]
+let sum =0 
+for(let i=0;i<11;i++){
+    ele[i]=parseInt(prompt("enter element"))
+    
+    if(ele[i] %2 !=0){
+     console.log(ele[i]);
+     
+    }
+}
+
+
